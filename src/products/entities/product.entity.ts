3ff8e10dd7a471c836b996fddea889b5505fda35
Benavides Michael <Benavides.michael.munoz@gmail.com>
2023-10-06 08:50:58 +0200

    product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   name: string;
+
+  @Column({ type: 'varchar', length: 500 })
   description: string;
+
+  @Column({ type: 'decimal' })
   price: number;
+
+  @Column({ type: 'int' })
   stock: number;
+
+  @Column()
   image: string;
 }
