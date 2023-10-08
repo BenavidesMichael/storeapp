9b46c1b7df36b5ecb3427233baf61af1f9db9cf9
Benavides Michael <Benavides.michael.munoz@gmail.com>
2023-10-08 12:48:51 +0200

    cors *

--- src/main.ts
+++ src/main.ts
@@ -22,9 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
   // ----------------- CORS -----------------
-  app.enableCors((opt) => {
-    opt.origin = ['http://localhost:4200'];
-  });
+  app.enableCors();
   // ----------------- server -----------------
   await app.listen(3000);
 }
